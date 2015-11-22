e5a0c498aa864c7d088ee5b2590abd601808fb26
Tamim Sookoor <tamimsookoor@gmail.com>
2015-11-21 20:34:23 -0500

    Exercise 1.2 done

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -13,12 +13,50 @@ import sys
 import nsfg
 import thinkstats2
 
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz'):
+    """Reads the NSFG respondent data.
+
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    return dct.ReadFixedWidth(dat_file, compression='gzip')
+
+def ValidatePregnum(resp):
+    """Validate pregnum in the respondent file.
+
+    resp: respondent DataFrame
+    """
+
+    # read the pregnancy frame
+    df = nsfg.ReadFemPreg() 
+
+    # make the map from caseid to list of pregnancy indices
+    preg_map = nsfg.MakePregMap(df)
+
+    # iterate through the respondent pregnum series
+    for index, pregnum in resp.pregnum.items():
+
+        # check that pregnum from the respondent file equals
+        # the number of records in the pregnancy file
+        if pregnum != len(preg_map[resp.caseid[index]]):
+            return False
+    return True
 
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
+    df = ReadFemResp()
+
+    assert(len(df) == 7643)
+    assert(df.pregnum.value_counts()[1] == 1267)
+    assert(ValidatePregnum(df))
+
     print('%s: All tests passed.' % script)
 
 
