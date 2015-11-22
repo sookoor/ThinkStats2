539e05820dd15b4f391eaab530df5533b4a71a8f
Tamim Sookoor <tamimsookoor@gmail.com>
2015-11-22 01:08:54 -0500

    Chapter 2 exercises done

--- code/chap02ex.py
+++ code/chap02ex.py
@@ -13,7 +13,6 @@ from operator import itemgetter
 import first
 import thinkstats2
 
-
 def Mode(hist):
     """Returns the value with the highest frequency.
 
@@ -21,7 +20,8 @@ def Mode(hist):
 
     returns: value from Hist
     """
-    return 0
+    _, value = max([(freq, value) for value, freq in hist.Items()])
+    return value
 
 
 def AllModes(hist):
@@ -31,8 +31,8 @@ def AllModes(hist):
 
     returns: iterator of value-freq pairs
     """
-    return []
-
+    return sorted(hist.Items(), key=itemgetter(1), reverse=True)
+  
 
 def main(script):
     """Tests the functions in this module.
@@ -41,7 +41,15 @@ def main(script):
     """
     live, firsts, others = first.MakeFrames()
     hist = thinkstats2.Hist(live.prglngth)
+ 
+    # explore the weight difference between first babies and others
+    print('CohenEffectSize of weight: ' + str(thinkstats2.CohenEffectSize(firsts['totalwgt_lb'],
+                others['totalwgt_lb']))) 
 
+    # Explore the difference in pregnancy length between first babies and
+    # others.
+    print('CohenEffectSize of pregnancy length: ' + str(thinkstats2.CohenEffectSize(firsts['prglngth'], others['prglngth'])))  
+   
     # test Mode    
     mode = Mode(hist)
     print('Mode of preg length', mode)
